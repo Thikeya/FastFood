ee0a861a37c5f9364e05d6ec33227b43121f52ad
Thikeya <69624852+Thikeya@users.noreply.github.com>
2021-06-06 21:05:07 -0400

    adicionando os metodos dao das classes

--- src/model/dao/Item_pedidoDao.java
+++ src/model/dao/Item_pedidoDao.java
@@ -1,5 +1,13 @@
 package model.dao;
 
-public interface Item_pedidoDao {
+import java.util.List;
 
+import model.entities.Item_Pedido;
+
+public interface Item_PedidoDao {
+	void insert(Item_Pedido obj);
+	void update(Item_Pedido obj);
+	void deleteById(Integer id);
+	Item_Pedido findById(Integer id);
+	List<Item_Pedido> findAll();
 }
